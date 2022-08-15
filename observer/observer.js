c3262c1aa7db9bbf98c10d1ec2074dbeb2c6adb8
moto4321 <moto432195@gmail.com>
2022-08-15 17:57:26 +0900

    observer pattern

---
+++ observer/observer.js
@@ -0,0 +1,52 @@
+class Subject {
+  constructor() {
+    this.observers = [];
+  }
+  getObserversList() {
+    return this.observers;
+  }
+  subscribe(observer) {
+    this.observers.push(observer);
+  }
+  unsubscribe(observer) {
+    this.observers = this.observers.filter((obs) => obs !== observer);
+  }
+  notifyAll() {
+    this.observers.forEach((subscriber) => {
+      try {
+        subscriber.update(this.constructor.name);
+      } catch (err) {
+        console.error("error", err);
+      }
+    })
+  }
+}
+
+class Observer {
+  constructor(name) {
+    this.name = name;
+  }
+
+  update(subj) {
+    console.log(`${this.name}: notified from ${subj} class!`)
+  }
+}
+
+const subj = new Subject();
+
+const a = new Observer("A");
+const b = new Observer("B");
+const c = new Observer("C");
+
+subj.subscribe(a);
+subj.subscribe(b);
+subj.subscribe(c);
+
+console.log(subj.getObserversList());
+
+subj.notifyAll();
+
+subj.unsubscribe(c);
+
+console.log();
+subj.notifyAll();
